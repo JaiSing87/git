786340e075a023113ecbcf0cded53703a1eb4e13
Jai Singh <jaiprof@gmail.com>
2014-09-05 15:18:24 +0530

    New Changes to TestClassForCommit

--- TestProject/src/TestClassForCommit.java
+++ TestProject/src/TestClassForCommit.java
@@ -4,6 +4,7 @@ public class TestClassForCommit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Commit Changes Done");
+		System.out.println("New Changes Done");
 	}
 
 }
