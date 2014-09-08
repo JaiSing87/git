bc4f4ae032e78506b836398fceb5a1a62d981f91
Jai Singh <jaiprof@gmail.com>
2014-09-08 12:09:12 +0530

    Commiting new changes

--- TestProject/src/TestClassForCommit.java
+++ TestProject/src/TestClassForCommit.java
@@ -1,10 +1,15 @@
 
 public class TestClassForCommit {
 
+	static int x =0;
+	final int y = 0;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
 		System.out.println("Commit Changes Done");
 		System.out.println("New Changes Done");
+		System.out.println("Doing some new changes");
 	}
 
 }
