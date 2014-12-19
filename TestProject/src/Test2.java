0bfead4e4ef653549ca75f04cb22f1f7039151c4
Jai Singh <jaiprof@gmail.com>
2014-12-19 11:26:55 +0530

    print line

---
+++ TestProject/src/Test2.java
@@ -0,0 +1,14 @@
+
+public class Test2 extends TestClassForCommit{
+
+	public Test2(){
+		super();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Change Done");
+		
+	}
+
+}
