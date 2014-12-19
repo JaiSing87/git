582bdf6e31447ed68f8eb71a3bab32fa6ca74209
Jai Singh <jaiprof@gmail.com>
2014-12-19 11:29:54 +0530

    22

--- TestProject/src/Test2.java
+++ TestProject/src/Test2.java
@@ -7,7 +7,7 @@ public class Test2 extends TestClassForCommit{
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Change Done");
+		System.out.println("Change Done!");
 		
 	}
 
